bd0528fe73c4c21f7f70782e38c134b1a715207d
yom-ko <artem.getcode@gmail.com>
2018-07-21 13:02:00 +0300

    [Stories] Minor improvement of the fetching api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,14 @@
 export const fetchArticles = url =>
-  fetch(url).then(response => response.json());
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Probably, a network error occurred.');
+    })
+    .catch(error => {
+      console.log('A problem with the fetch operation: ', error.message);
+    });
 
 export const url =
   'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=111e8ebd07184c4290fce4a5e5f390a6';
